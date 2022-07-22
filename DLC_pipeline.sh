eca6ed78caffaf805e2135180e5a6a09cabc0d8a
Grégoire Yasmine-Degobert <gydegobert@hpc-gw1.hpc.swc.ucl.ac.uk>
2022-07-22 15:41:16 +0000

    reviewed bash script

--- DLC_pipeline.sh
+++ DLC_pipeline.sh
@@ -1,15 +1,14 @@
+#!/bin/bash
+
 #SBATCH -p gpu # partition (queue)
-#SBATCH -N 1   # number of nodes
-#SBATCH --mem-per-cpu 4000 # memory pool for all cores
+#SBATCH --gres gpu:4
 #SBATCH -t 0-4:00 # time (D-HH:MM)
 #SBATCH -o slurm.%N.%j.out # STDOUT
 #SBATCH -e slurm.%N.%j.err # STDERR
-#SBATCH --gres gpu:4
-#SBATCH --cpu_per_task 1
-#SBATCH --array 0:38
+#SBATCH --array 0-38
 
-module load conda
-module load cuda
+module load miniconda
+module load cuda/11.6
 conda activate aeon_env
 python export_video_data.py $SLURM_ARRAY_TASK_ID
 
