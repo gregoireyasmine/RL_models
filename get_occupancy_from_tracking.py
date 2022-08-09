be0f3f23c0c9efdceb730e290619018921748ddf
gregoireyasmine <85488430+gregoireyasmine@users.noreply.github.com>
2022-08-09 19:37:13 +0200

    Update get_occupancy_from_tracking.py

--- get_occupancy_from_tracking.py
+++ get_occupancy_from_tracking.py
@@ -6,7 +6,7 @@ from math import sqrt
 data_path = '/nfs/winstor/delab/data/arena0.1/socialexperiment0_preprocessed/'
 ROI_distance_quotient = 0.17
 sessions = listdir(data_path)
-df = pd.DataFrame()
+df = pd.DataFrame(columns = ['frame', 'patch_occupied'])
 
 for sess in sessions:
     filename = data_path + sess + listdir(data_path + sess)[0]
@@ -21,14 +21,28 @@ for sess in sessions:
         lwy = sessdf.iloc[frame]['single_left_wheel_y']
         frame += 1
     distance_between_patches = sqrt((rwx - lwx)**2 + (rwy - lwy)**2)
-
-
+    patch_radius = ROI_distance_quotient * distance_between_patches
+    for i, frame in sessdf.itertuples():
+        x = frame.ind1_nose_x 
+        y = frame.ind1_nose_y
+        if x and y:
+            patch_occupied = is_on_patch(rwx, rwy, lwx, lwy, x, y, patch_radius)
+        else: 
+            patch_occupied = None
+        df.append({'frame': i, 'patch_occupied': patch_occupied})
+                   
+            
 def belong_to_circle(ptx, pty, cx, cy, radius):
     """ Returns if a point belongs to a circle. """
     return (ptx - cx)**2 + (pty - cy)**2 < radius**2
 
 
-def is_on_patch(patch1_coords, patch2_coords):
+def is_on_patch(patch1_x, patch1_y, patch2_x, patch2_y, ind_x, ind_y, radius):
     """ Returns i if the individual is on patch i """
-    for i, patch_coords in enumerate([patch1_coords, patch2_coords]):
+    for i, px, py in enumerate([(patch1_x, patch1_y), (patch2_x, patch2_y)]):
+        if belong_to_circle(ind_x, ind_y, px, py, radius):
+            return i+1
+    return 0
+        
+        
 
