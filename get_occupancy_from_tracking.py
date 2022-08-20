b42a449058a20b6d1c477f824577515f806bb965
Gregoire Degobert Yasmine <gregoire.yd@gmail.com>
2022-08-20 17:48:40 +0200

    fix dlc

--- get_occupancy_from_tracking.py
+++ get_occupancy_from_tracking.py
@@ -1,6 +1,8 @@
 from os import listdir
 import pandas as pd
 from math import sqrt
+import numpy as np
+
 ## This is meant to be ran on HPC
 
 data_path = '/nfs/nhome/live/gydegobert/repos/RL_models/dlcdata/'
@@ -8,15 +10,29 @@ ROI_distance_quotient = 0.17
 sessions = listdir(data_path)
 dict = {}
 
-for i, sess in enumerate(sessions):
-    df = pd.DataFrame(columns = ['frame', 'patch_occupied'])
 
+def belong_to_circle(ptx, pty, cx, cy, radius):
+    """ Returns if a point belongs to a circle. """
+    return (ptx - cx) ** 2 + (pty - cy) ** 2 < radius ** 2
+
+
+def is_on_patch(patch1_x, patch1_y, patch2_x, patch2_y, ind_x, ind_y, radius):
+    """ Returns i if the individual is on patch i """
+    for i, p in enumerate([(patch1_x, patch1_y), (patch2_x, patch2_y)]):
+        px, py = p
+        if belong_to_circle(ind_x, ind_y, px, py, radius):
+            return i + 1
+    return 0
+
+
+for i, sess in enumerate(sessions):
+    df = pd.DataFrame(columns=['frame', 'patch_occupied'])
     filename = data_path + sess
     sessdf = pd.read_csv(filename)
 
     rwx, rwy, lwx, lwy = None, None, None, None
     frame = 0
-    while sum([type(w) == float for w in (rwx, rwy, lwx, lwy)]) != 4:
+    while sum([type(w) == np.float64 for w in (rwx, rwy, lwx, lwy)]) != 4:
         rwx = sessdf.iloc[frame]['single_right_wheel_x']
         rwy = sessdf.iloc[frame]['single_right_wheel_y']
         lwx = sessdf.iloc[frame]['single_left_wheel_x']
@@ -24,28 +40,17 @@ for i, sess in enumerate(sessions):
         frame += 1
     distance_between_patches = sqrt((rwx - lwx)**2 + (rwy - lwy)**2)
     patch_radius = ROI_distance_quotient * distance_between_patches
-    for i, frame in sessdf.itertuples():
+    for i, frame in enumerate(sessdf.itertuples()):
         x = frame.ind1_nose_x 
         y = frame.ind1_nose_y
         if x and y:
             patch_occupied = is_on_patch(rwx, rwy, lwx, lwy, x, y, patch_radius)
         else: 
             patch_occupied = None
-        df.append({'frame': i, 'patch_occupied': patch_occupied})
+        df = pd.concat([df, pd.DataFrame({'frame': i, 'patch_occupied': patch_occupied}, index=[0])], ignore_index=True)
     dict[i] = df
 
             
-def belong_to_circle(ptx, pty, cx, cy, radius):
-    """ Returns if a point belongs to a circle. """
-    return (ptx - cx)**2 + (pty - cy)**2 < radius**2
 
-
-def is_on_patch(patch1_x, patch1_y, patch2_x, patch2_y, ind_x, ind_y, radius):
-    """ Returns i if the individual is on patch i """
-    for i, px, py in enumerate([(patch1_x, patch1_y), (patch2_x, patch2_y)]):
-        if belong_to_circle(ind_x, ind_y, px, py, radius):
-            return i+1
-    return 0
-        
         
 
