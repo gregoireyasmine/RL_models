9b8e1e0a20de3dc61b0d41441288311ab6a7c9ae
Gregoire Degobert Yasmine <gregoire.yd@gmail.com>
2022-08-21 15:37:33 +0200

    added dlc analysis task

---
+++ compute_occupancy.py
@@ -0,0 +1,11 @@
+import numpy as np
+from os import listdir
+
+sessions = listdir('./dlcoccdata/')
+occupancy = {}
+for session in sessions:
+    data = np.load('./dlcoccdata/'+session, allow_pickle=True)[0]
+    data = data[:, 1]
+    occ = [np.sum(len(data == k))/len(data) for k in range(2)]
+    occupancy[session] = occ
+np.save('dlcoccupancydata', occupancy)
--- plot_sim_data.py
+++ plot_sim_data.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import ast
 import matplotlib as mpl
+from math import log, pi
 COLORS = mpl.colors.CSS4_COLORS
 
 # HPC path (Gregoire data)
@@ -19,29 +20,44 @@ fig, ax = plt.subplots(3)
 
 
 def scat(axes, alpha, beta, gamma, color, label):
-    sims_to_plot = sims.loc[sims['gamma'] == alpha]
+    sims_to_plot = sims.loc[sims['alpha'] == alpha]
     sims_to_plot = sims_to_plot.loc[sims['beta'] == beta]
-    sims_to_plot = sims_to_plot.loc[sims['alpha'] == gamma]
+    sims_to_plot = sims_to_plot.loc[sims['gamma'] == gamma]
     for occ in sims_to_plot['occ']:
         occ = ast.literal_eval(occ)
         axes[0].scatter(occ[0], occ[2], color=color, label=label)
         axes[1].scatter(occ[2], occ[3], color=color, label=label)
         axes[2].scatter(occ[0], occ[3], color=color, label=label)
 
-
+'''
 for i, alpha in enumerate(np.arange(0.01, 1, 0.01)):
     color = COLORS[list(COLORS.keys())[i]]
     beta = 0.2
     gamma = 0.2
     scat(ax, alpha, beta, gamma, color=color, label='alpha = ' + str(alpha))
+'''
+
+# plt.show()
 
 
-plt.show()
+def gaussian3d_fit(occ):
+    mean = np.mean(occ, axis=0)
+    cov = np.cov(occ, rowvar=0)
+    print(mean, cov)
+
+
+def log_likelihood(x, mean, cov):
+    det = np.linalg.det(cov)
+    return -0.5 * (log(det) + np.dot(np.dot((x - mean), np.linalg.inv(cov)), (x-mean)) + 3*log(2*pi))
+
+
+def compute_occupancy():
+    
+
 
 
 """
 for alpha in np.arange(0.01, 1, 0.01):
     for beta in np.arange(0.1, 1, 0.1):
         for gamma in np.arange(0.1, 1, 0.1):
-            
 """
