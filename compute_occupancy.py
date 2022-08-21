ebaf0b1a602f20d0fc8b29b8458febcc0698533c
Gregoire Degobert Yasmine <gregoire.yd@gmail.com>
2022-08-21 16:32:21 +0200

    fix dlc occupancy

--- compute_occupancy.py
+++ compute_occupancy.py
@@ -6,6 +6,6 @@ occupancy = {}
 for session in sessions:
     data = np.load('./dlcoccdata/'+session, allow_pickle=True)
     data = data[:, 1]
-    occ = [np.sum(len(data == k))/len(data) for k in range(2)]
+    occ = [np.sum(data == k)/len(data) for k in range(2)]
     occupancy[session] = occ
 np.save('dlcoccupancydata', occupancy)
